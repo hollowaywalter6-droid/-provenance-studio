efea1168832a779ad54f465abd87806d478f64db
hollowaywalter6-droid <hollowaywalter6@gmail.com>
2026-09-24 18:12:51 -0700

    Cache privacy disclosure for offline iOS use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='provenance-v4-7-0';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./privacy.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('provenance-')&&k!==CACHE).map(k=>caches.delete(k))))
